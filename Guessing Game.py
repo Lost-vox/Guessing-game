ced65234711ff51e0c2142c2f6fee5c3a2075094
Lost-vox <37073066+Lost-vox@users.noreply.github.com>
2018-04-18 09:26:02 -0400

    Update Guessing Game.py

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,6 +1,19 @@
 import random
-theNumber = random
 
-while True:
-  userInput = input("Guess a number?")
+guesses = 0
+
+print("Hey what is your name?")
+yourname = input()
+
+theNumber = random.randint(1, 10)
+print('alrighty then,' + yourname + 'I have a nummber between 1 and 10 try to guess it in 5 guesses.')
+
+while guesses < 5:
+  print('Take a guess')
+  guess = input()
+  guess = int(guess)
   
+  guesses = guesses + 1
+  
+  if guess < theNumeber:
+    print(
